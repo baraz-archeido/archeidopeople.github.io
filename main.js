427d0ac0fc2720b0817e32242fdbd6add8416ded
baraz-archeido <lucabaradel@archeido.com>
2025-01-08 16:38:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -69,14 +69,13 @@ function selectPeople() {
   const currentDate = new Date();
   const currentWeek = getWeekNumber(currentDate);
 
-  let trashShiftIndex = null ? 0 : currentWeek % trashPeople.length;
-  let coffeeShiftIndex = null ? 0 : currentWeek % coffeePeople.length;
+  let shiftIndex = null ? 0 : currentWeek % trashPeople.length;
 
   // Select one person for trash and another for machine cleaning
-  const trashPerson = trashPeople[(trashShiftIndex + 0) % trashPeople.length];
-  const nextTrashPerson = trashPeople[(trashShiftIndex + 1 ) % trashPeople.length];
-  const machinePerson = coffeePeople[(coffeeShiftIndex + 3) % coffeePeople.length];
-  const nextMachinePerson = coffeePeople[(coffeeShiftIndex + 4) % coffeePeople.length];
+  const trashPerson = trashPeople[(shiftIndex + 0) % trashPeople.length];
+  const nextTrashPerson = trashPeople[(shiftIndex + 1 ) % trashPeople.length];
+  const machinePerson = coffeePeople[(shiftIndex + 4) % coffeePeople.length];
+  const nextMachinePerson = coffeePeople[(shiftIndex + 5) % coffeePeople.length];
 
   // Display the results
   document.getElementById("trash").innerText = trashPerson;
